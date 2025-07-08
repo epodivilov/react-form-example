98646b473ffbe6ab20042cd3d2bff68df300980d
Evgeniy Podivilov <evgeniy.podivilov@gmail.com>
2025-07-08 15:07:11 +0100

    feat: 🚀 Configure Vite and add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-});
+  base: mode === "production" ? "/react-form-example/" : "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+}));
